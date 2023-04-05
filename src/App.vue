a8278f877c3cfb1a43bbdf3bf6914b45dd40f45f
dikshya <dikshyabhusal48@gmail.com>
2023-04-05 13:19:18 +0545

    jwellery project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+
+<header>
+        <div class="wrapper wrap">
+            <ul class="left-nav">
+                <li>Home</li>
+                <li>Products</li>
+            </ul>
+    
+            <div class="logo">
+                <img src="./assets/logo.svg" alt="Jewelry Logo" title="Jewelry Logo" />
+            </div>
+    
+            <ul class="right-nav">
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </header>
+
+    <section class="hero">
+        <div class="wrapper wrap">
+            <div class="image">
+                <img src="./assets/banner-image.jpg" alt="Jewelry">
+            </div>
+
+            <div class="content">
+                <h1>Premium Handmade Jewellry On Every Occassion</h1>
+                <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying.</p>
+                <a href="#" class="btn-primary">
+                    <p>Find Out More</p>
+                </a>
+            </div>
+
+        </div>
+    </section>
+
+</template>
